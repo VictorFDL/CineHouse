f8f353da7411e2ab1b2d3cca8735a32929736d24
VictorFDL <v.cremasco05@gmail.com>
2022-08-09 14:05:13 -0300

    Finalizada função buscarFilme()

--- cinema.js
+++ cinema.js
@@ -22,28 +22,32 @@ let catalogo = [{
 
 //  **** FUNÇÃO PARA ****
 //  Adicionar um filme (Objeto) ao array "catalogo"
-function adicionarFilme(codigo,titulo, duracao, atores, anoDeLancamento, emCartaz ){
-let novoFilme = {
-    codigo: codigo,
-    titulo: titulo,
-    duracao: duracao,
-    atores: atores,
-    anoDeLancamento: anoDeLancamento,
-    emCartaz: emCartaz
-   }
-   catalogo.push(novoFilme);
+function adicionarFilme(codigo, titulo, duracao, atores, anoDeLancamento, emCartaz) {
+    let novoFilme = {
+        codigo: codigo,
+        titulo: titulo,
+        duracao: duracao,
+        atores: atores,
+        anoDeLancamento: anoDeLancamento,
+        emCartaz: emCartaz
+    }
+    catalogo.push(novoFilme);
 }
-//adicionarFilme(3, "Até o Último Homem", 139, ["Andrew Garfield", "Vince Vaughn", "Luke Bracey", "Sam Worthington"],2016, false)
+adicionarFilme(3, "Até o Último Homem", 139, ["Andrew Garfield", "Vince Vaughn", "Luke Bracey", "Sam Worthington"],2016, false)
 
 //  **** FUNÇÃO PARA ****
 //Buscar um filme dentro do array "catalago" através do número do 'codigo'
-function buscarFilmes(codigo){
-    return codigo.codigo == 1
+function buscarFilme(cod) {
+    let filmeEscolhido = catalogo.find(el => el.codigo == cod)
+    if(cod > catalogo.length){
+        console.log("Número de código identificador não existente em nosso catálogo. Tente um numero entre 1 e " + catalogo.length)
+    }else{
+        console.log(`O filme buscado foi "${filmeEscolhido.titulo}", sua duração é de ${filmeEscolhido.duracao} minutos. Seu ano de lançamento é: ${filmeEscolhido.anoDeLancamento}`)
+    }
 }
-let filmeEscolhido = catalogo.find(buscarFilmes(1))
-console.log(`O filme buscado foi "${filmeEscolhido.titulo}", sua duração é de ${filmeEscolhido.duracao} minutos. Seu ano de lançamento é: ${filmeEscolhido.anoDeLancamento}`)
 
-console.log("Psyke lindo!")
+buscarFilme(5)
+
 //   ****FUNÇÃO PARA  ****
 // A função deve receber como parâmetro o número identificador do filme
 // escolhido, buscar o filme com base no parâmetro recebido e alterar o status
